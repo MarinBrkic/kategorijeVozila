3b03f556154982cfa47f65ee69b5e68a046769f1
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-10-02 09:28:19 +0200

    Add files via upload

---
+++ kategorijeVozila1/kategorijeVozila1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace kategorijeVozila1
+{
+    public partial class Form1 : Form
+    {
+        List<Vozilo> listaVozila = new List<Vozilo>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            Vozilo vozilo = new Vozilo(txtModel.Text, Convert.ToInt16(txtGodinaProizvodnje.Text),
+                Convert.ToInt16(txtBrojKotaca.Text));
+
+            txtModel.Clear();
+            txtGodinaProizvodnje.Clear();
+            txtBrojKotaca.Clear();
+            txtModel.Focus();
+
+            DialogResult upit = MessageBox.Show("Želite li unesti još podataka?", "Upit",
+    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            switch (upit)
+            {
+                case DialogResult.Yes:
+                    {
+                        listaVozila.Add(vozilo);
+                        break;
+                    }
+                case DialogResult.No:
+                    {
+                        listaVozila.Add(vozilo);
+                        txtModel.Enabled = false;
+                        txtGodinaProizvodnje.Enabled = false;
+                        txtBrojKotaca.Enabled = false;
+
+                        break;
+                    }
+            }
+        }
+
+        private void btnIspis_Click(object sender, EventArgs e)
+        {
+            string putanjaDoDatoteke = "C:\\Users\\Ucenik\\Documents\\TestCSV1";
+            try
+            {
+                // Stvaramo ili otvaramo datoteku za pisanje
+                using (StreamWriter sw = new StreamWriter(putanjaDoDatoteke))
+                {
+                    // Zaglavlje (imena stupaca)
+                    sw.WriteLine("Model,GodinaProizvodnje,BrojKotaca");
+
+                    // Iteriramo kroz listu osoba i zapisujemo podatke u datoteku
+                    foreach (Vozilo vozilo in listaVozila)
+                    {
+                        sw.WriteLine(vozilo.ToString());
+                    }
+                }
+
+                MessageBox.Show("Podaci su uspješno spremljeni u CSV datoteku!", "Uspjeh",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Došlo je do pogreške prilikom spremanja podataka: " + ex.Message,
+                    "Pogreška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            foreach (Vozilo vozilo in listaVozila)
+            {
+                txtIspis.AppendText(vozilo.ToString());
+            }
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+            foreach (Vozilo vozilo in listaVozila)
+            {
+                if (vozilo.BrojKotaca == 2)
+                {
+                    vozilo.Kategorija = "Motocikl";
+                }
+                else if (vozilo.BrojKotaca == 4)
+                {
+                    vozilo.Kategorija = "Automobil";
+                }
+                else
+                {
+                    vozilo.Kategorija = "Kamion";
+                }
+            }
+
+
+        }
+        }
+    }
---
+++ kategorijeVozila1/kategorijeVozila1/Vozilo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kategorijeVozila1
+{
+    internal class Vozilo
+    {
+        string model, kategorija;
+        int godinaProizvodnje, brojKotaca;
+
+        public Vozilo(string model, int godinaProizvodnje, int brojKotaca)
+        {
+            Model = model;
+            GodinaProizvodnje = godinaProizvodnje;
+            BrojKotaca = brojKotaca;
+        }
+
+        public string Model { get => model; set => model = value; }
+        public int GodinaProizvodnje { get => godinaProizvodnje; set => godinaProizvodnje = value; }
+        public int BrojKotaca { get => brojKotaca; set => brojKotaca = value; }
+
+        public string Kategorija { get => kategorija; set => kategorija = value; }
+
+        public override string ToString()
+        {
+            //string ispis = this.Ime + "," + this.Prezime + "," + this.Email + "," + this.GodinaRodjenja;
+
+            string ispis = $"{this.Model},{this.GodinaProizvodnje},{this.BrojKotaca},{this.Kategorija}";
+
+            return ispis;
+        }
+    }
+}
